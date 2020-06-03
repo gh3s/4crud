d937005ece83c83617d815d44cb6779100419a76
ghsss <guilhermehsss@gmail.com>
2020-06-03 10:54:09 -0300

    corrected routing filter

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ class Crud {
             if (body !== '') { req.body = JSON.parse(body) } // TODO: Implement another acceptable methods than JSON
             const urlParams = new URL(req.url, `http://${req.headers.host}`)
             req.search = urlParams.searchParams
-            if (this.url === req.url) { // route filtering (if filter is equal to url, proceed)
+            console.log(urlParams.pathname)
+            if (this.url === urlParams.pathname) { // route filtering (if filter is equal to url pathname (without ?'), proceed)
               this[req.method](req, res)
             } else { res.end('invalid route') }
           } catch (e) {
